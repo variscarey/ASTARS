64d24cacb7259c715a1f2c63eb1e897653d1af8d
Varis Carey <variscarey@googlemail.com>
2020-05-20 16:04:08 -0600

    shifting some parts out of file, minor edits

--- astars/astars.py
+++ astars/astars.py
@@ -132,25 +132,6 @@ def STARS(x_init,F,mu_star,h,active=None,mult=False,wts=None):
 		#return [x_init, f0, y, g, x_init, f1, L_1_B]
 
 
-def train_rbf(xtrain,ftrain,noise=None,cutoff=.9):
-    from active_subspaces.utils.response_surfaces import PolynomialApproximation
-    from active_subspaces.utils.response_surfaces import RadialBasisApproximation 
-    from active_subspaces.utils.response_surfaces import RadialBasisApproximation 
-    
-    ss=ac.subspaces.Subspaces()
-    dim=xtrain.shape[1]
-    if ftrain.size > (dim+1)*(dim+2)/2:
-        rb_approx=RadialBasisApproximation(N=2)
-        #print('Quadratic Monomials')
-    else:
-        rb_approx=RadialBasisApproximation(N=1)
-    if noise==None:
-        rb_approx.train(xtrain,ftrain) 
-    else:
-        rb_approx.train(xtrain,ftrain,v=noise*np.ones(ftrain.shape))
-    [temp,df]=rb_approx.predict(xtrain,compgrad=True)
-    ss.compute(df=df)
-    return ss,rb_approx
 
 ## Here, slice from initial data
 def astars_update_active(func,xinit,noise,L_1,finit=None,max_it=100,update_per=5,verbose=True):
