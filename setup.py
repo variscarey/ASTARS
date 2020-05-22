283339e6e79d91b9a4181284b6560f4f30ecf32b
Varis Carey <variscarey@googlemail.com>
2020-05-22 15:18:00 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='ASTARS',
+      version='0.0.1',
+      description='derivative free optimizer exploiting the active subspace',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics'
+      ],
+      keywords='dimension reduction mathematics active subspaces uncertainty quantification optmization',
+      url='https://github.com/variscarey/ASTARS,
+      author='Varis Carey and Jordan Hall',
+      author_email='variscarey@googlemail.com',
+      license='MIT',
+      packages=['active_subspaces', 'active_subspaces.utils'],
+      install_requires=[
+          'numpy',
+          'scipy >= 0.15.0',
+          'matplotlib',
+          'active_subspaces_py3'
+      ],
+      #test_suite='nose.collector',
+      #tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
