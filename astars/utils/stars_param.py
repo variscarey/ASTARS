minor errors corrected in ECnoise.  Added ridge regression with variance penalty for determining better L1

astars/utils/stars_param.py
@@ -11,13 +11,13 @@ Created on Sat May 23 05:50:46 2020
 
 ## Inputs:  f: noisy black-box function; 
 ##          x_b: base point, Px1 col vec;
-##	    h: discretization level which is 0.01 by default;
+##        h: discretization level which is 0.01 by default;
 ##          M: number of samples to use, M must be >3 and default is 7 (suggested in paper); 
 ##          mult: a boolean handle for whether we have add/mult noise.
-##	    (We default to additive noise, so we set mult=False. User can set mult=True if needed.)
+##        (We default to additive noise, so we set mult=False. User can set mult=True if needed.)
 
 ## Outputs: sigma_hat: an estimator to the variance in noise of f;
-##	    xvals: the M x-values stored as an PxM array
+##        xvals: the M x-values stored as an PxM array
 ##          fvals: the M function values stored as an Mx1 column vector
 
 ## Cost:    M evals of f
@@ -25,113 +25,161 @@ Created on Sat May 23 05:50:46 2020
 # We only need numpy for ECNoise
 import numpy as np
 
-def ECNoise(f, x_b, h=0.01, M=7, mult_flag=False):
-
-	# Start with sigma_hat at 0
-	sigma_hat = 0
-
-	# Throw error for M too small
-	if M<=3:
-		return print('Please choose M>3.')
-
-	#reshape if row vector
-	x_b.reshape(-1,1)
-	# Grab the dimension of the domain of f
-
-	P = x_b.shape[0]
-
-	# Hard-coded normalized direction to sample in (either random or np.ones)
-	#if x_b.ndim > 1:
-	p_u = np.random.rand(P,1)
-	#p_u = np.ones((P,1))
-	#else:
-	#	p_u = np.random.rand(P)
-	#p_u = np.ones(P)
-	# Normalize
-	p = p_u/np.linalg.norm(p_u)
-
-	# Initiate storage for x values (xvals) and their f values (fvals)
-	xvals = np.copy(x_b)
-	fvals = np.zeros((M,1))
-
-	# Form and store the x/f values
-	for i in range(0,M):
-		intx = x_b + (i*h)*p
-		if i>=1:
-			xvals = np.hstack((xvals,intx))
-		else:
-			xvals = xvals
-		fvals[i,0] = f(intx)
-
-	# Test h
-	fmin = np.min(fvals); fmax = np.max(fvals)
-	diff = fmin - fmax
-	if diff > np.max([np.abs(fmin),np.abs(fmax)])*0.1:
-		# return print('h is too large. Try h/100 next. Default is h=0.01.') 
-		sigma_hat = [-1]
-		return sigma_hat
-
-	# Form difference table
-	gamma = 1
-	diff_col = np.copy(fvals) # purposely overwritten at each step j below
-	est = np.zeros((M-1,1)) # stores estimators
-	d_sign = np.zeros((M-1,1)) # entry j will be 1 if there are sign changes, 0 if not
-	
-	for j in range(0,M-1):
-		for i in range(0,M-j-1):
-			diff_col[i,0] = diff_col[i+1,0] - diff_col[i,0]
-
-		# h is too small when more than half the function values are equal (cite: More' and Wild)
-		if j == 1 and np.shape(np.nonzero(diff_col))[1] < M/2:
-			# return print('h is too small. Try 100*h next. Default is h=0.01.')
-			sigma_hat = [-2]
-			return sigma_hat
-		else:
-			h=h
-		
-		# Compute the estimates for the noise level
-		gamma = 0.5*((j+1)/(2*(j+1)-1))*gamma
-		est[j,0] = np.sqrt(gamma*np.mean(diff_col[0:M-j,0])**2)
-
-		# Determine sign changes
-		emin = np.min(diff_col[0:M-j,0]); emax = np.max(diff_col[0:M-j,0])
-		if emin*emax<0:
-			d_sign[j,0] = 1
-		else:
-			d_sign[j,0] = 0
-
-	# Determine noise level
-	for k in range(0,M-3):
-		dmin = np.min(est[k:k+2,0]); dmax = np.max(est[k:k+2,0])
-		if dmax <= 4*dmin and d_sign[k,0] == 1:
-			sigma_hat = est[j,0]
-			if mult is False:
-				noise_array = [sigma_hat, xvals, fvals]
-				return noise_array
-			else:
-				noise_array = [sigma_hat/fvals[0]**2, xvals, fvals] 
-				return noise_array
-		else:
-			sigma_hat = 0
-
-	# If we haven't found a sigma_hat, then h is too large (M and W)
-	if sigma_hat == 0:
-		# return print('h is too large. Try 0.01*h next. Default is h=0.01.')
-		sigma_hat = [-1]
-		return sigma_hat
 
 
 
+def ECNoise(f,x_b,h=1E-2,M=3,mult_flag=False):
+    '''
+    ECNoise: (f, x_b, h, M, mult) -> (sigma_hat, xvals, fvals, L1
 
-def get_L1(xdata,fdata,var):
+## Inputs:  f: noisy black-box function; 
+##          x_b: base point, Px1 col vec;
+##        h: discretization level which is 0.01 by default;
+##          M: number of samples to use, M must be >3 and default is 7 (suggested in paper); 
+##          mult: a boolean handle for whether we have add/mult noise.
+##        (We default to additive noise, so we set mult=False. User can set mult=True if needed.)
+
+## Outputs: sigma_hat: an estimator to the variance in noise of f;
+##        xvals: the M x-values stored as an PxM array
+##          fvals: the M function values stored as an Mx1 column vector
+
+## Cost:    M evals of f
+
+# We only need numpy for ECNoise
+    '''
+
+    
+    
+    # Start with sigma_hat at 0 (No Noise)
+    sigma_hat = 0
+
+    # Throw error for M too small
+    if M < 1:
+        return print('Please choose M>1.')
+    x_b=x_b.flatten()
+    #initialize points
+    xvals = np.empty((x_b.size,2*M+1))
+    fvals = np.empty(2*M+1)
+
+
+    # Hard-coded random normalized direction to sample in
+    p_u = np.random.randn(x_b.size)
+
+    # Normalize
+    p = p_u/np.linalg.norm(p_u)
+
+    # Form and store the x and f values
+    
+    index = 0
+    for i in np.arange(-M,M+1):
+        intx = x_b + (i)*p*h
+        xvals[:,index] = intx
+        fvals[index] = f(intx)
+        index += 1
+
+    gamma = 1.0
+    diff_col = np.zeros((2*M+1,2*M+1))
+    diff_col[:,0] = fvals
+    est= np.zeros(2*M)
+    
+    d_sign = np.zeros(2*M,dtype=np.bool_)
+    # entry j will be 1 if there are sign changes, 0 if not
+    
+    # Form difference table
+    for i in range(1,2*M+1):
+        if i==1:
+            diff_col[0:-i,i] = diff_col[1:,i-1] - diff_col[0:-i,i-1]
+            if np.sum(np.abs(diff_col[0:-i,i]) < np.finfo(float).tiny)  > M:
+                print(np.abs(diff_col[0:-i,i]) < np.finfo(float).tiny)
+                print('h is too small. Try 100*h next. Default is h=0.01.')
+                print('diff column',diff_col[:,i])
+                sigma_hat = [-2]
+                return sigma_hat
+        else:
+           diff_col[0:-i,i] = diff_col[1:-i+1,i-1] - diff_col[0:-i,i-1] 
+        
+        # Compute the estimates for the noise level
+        gamma *= 0.5 * i / (2 * (i-1) + 1) 
+        est[i-1] = np.sqrt(gamma * np.mean(diff_col[0:-i,i]**2))
+
+        # Determine sign changes
+        d_sign[i-1] = ((np.sum(diff_col[:,i] > 0)) and np.sum(diff_col[:,i] < 0) > 0)
+    
+    with (np.printoptions(precision = 4, suppress = True)):
+        print(diff_col)
+        print('noise estimates',est)
+
+
+    # Determine noise level
+    for k in range(0,2*M-2):
+        dmin = np.min(est[k:k+3])
+        dmax = np.max(est[k:k+3])
+        if dmax <= 4*dmin and d_sign[k] == 1:
+            sigma_hat = est[k]
+            if mult_flag is False:
+                noise_array = [sigma_hat, xvals, fvals]
+                level = k+1
+                print('Using difference level',k+1)
+                break
+                #return noise_array
+            else:
+                noise_array = [sigma_hat/fvals[0]**2, xvals, fvals] 
+                break
+                #return noise_array
+        else:
+            sigma_hat = 0
+
+    # If we haven't found a sigma_hat, then h is too large (M and W)
+    if sigma_hat == 0:
+        # return print('h is too large. Try 0.01*h next. Default is h=0.01.')
+        print(est)
+        print(d_sign)
+        sigma_hat = [-1]
+        return sigma_hat
+    
+    #NEW, DETERMINE L1 after noise found
+    f2d = fvals[0:-2]+fvals[2:]-2*fvals[1:-1]
+    print('difference quotients',f2d)
+    if np.max(np.abs(f2d)) > 100*noise_array[0]:
+        L1=np.max(f2d)/h**2
+    else:
+        print('Warning: h may be too small for accurate L1')
+        f2d=np.abs(fvals[0]+fvals[-1]-2*fvals[M])
+        L1=np.max(f2d)/(M*h)**2
+    print('L1=',L1)
+    return noise_array,L1,level
+    
+
+
+
+def get_L1(xdata,fdata,var,degree = 3):
     ''' 
     computes approximate L1 Lipschitz constant by using 1D polynomial
     '''
     
     import numpy as np
+    from .reg_ls import ls_l2reg
     
-    weight=var*np.ones(xdata.size)
-    degree=min(xdata.size-1,5)
-    poly=np.polynomial.polynomial.Polynomial.fit(xdata,fdata,degree,w=weight)
+    #scale data on (0,1)
+    map=1/(xdata[-1]-xdata[0])
+    
+    #vandermone matrix with normalized coefficeints
+    X=np.vander(xdata*map,N=degree+1,increasing=True)
+    coeff=ls_l2reg(X,var,fdata)
+  
+    poly=np.polynomial.Polynomial( coeff,domain=[xdata.min(),xdata.max()],window=[0,1])
+    
+    #TODO: put in non-grid search solve for L1.
     temp=(poly.deriv(2)).linspace()
+   
     return np.max(np.absolute(temp[1]))
+
+
+
+
+
+
+
+
+
+