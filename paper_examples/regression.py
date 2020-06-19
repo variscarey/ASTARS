f8df5fb12c6e0bbda9acf32ff98e5c44f47f0842
Jordan Hall <jordan.r.hall@ucdenver.edu>
2020-06-19 15:24:22 -0600

    added astars and trials

--- paper_examples/regression.py
+++ paper_examples/regression.py
@@ -7,6 +7,7 @@ Created on Fri Jun 19 09:24:41 2020
 
 import numpy as np
 from astars.stars_sim import Stars_sim
+import matplotlib.pyplot as plt
 
 
 def lin_f(pred,weights,var=1E-4):
@@ -55,25 +56,51 @@ def stars_wrapper(iterate,dim=10):
     weights = iterate[0:10]
     #print(weights)
     pred = iterate[10:].reshape(-1,dim)
-    print('initial first component',noisy_pred[:,0])
-    print('current first component',pred[:,0])
+    #print('initial first component',noisy_pred[:,0])
+    #print('current first component',pred[:,0])
     return rms_loss(weights,pred,data)
 
 #stars setup
 maxit = 500
 init_pt = np.hstack((init_weights,noisy_pred.flatten()))    
-test = Stars_sim(stars_wrapper, init_pt, L1 = 400.0, var = 1E-4, verbose = True, maxit = maxit)
-test.STARS_only = True
-test.debug = False
-test.get_mu_star()
-test.get_h()
-# do 100 steps
-while test.iter < test.maxit:
-   test.step()
+ntrials = 10
+f_avr = np.zeros(maxit+1)  #set equal to number of iterations + 1
 
-import matplotlib.pyplot as plt
-
-plt.semilogy(test.fhist)
-
-
+for trial in range(ntrials):
+    #sim setup
+    test = Stars_sim(stars_wrapper, init_pt, L1 = 400.0, var = 1E-4, verbose = False, maxit = maxit)
+    test.STARS_only = True
+    test.get_mu_star()
+    test.get_h()
+    # do 100 steps
+    while test.iter < test.maxit:
+        test.step()
     
+    #update average of f
+    f_avr += test.fhist  
+    
+f2_avr = np.zeros(maxit+1)
+
+for trial in range(ntrials):
+    #sim setup
+    test = Stars_sim(stars_wrapper, init_pt, L1 = 400.0, var = 1E-4, verbose = True, maxit = maxit)
+    #test.STARS_only = True
+    test.get_mu_star()
+    test.get_h()
+    # adapt every 10 timesteps using quadratic(after inital burn)
+    test.train_method = 'GQ'
+    test.adapt = 10 # Sets number of sub-cylcing steps
+    
+    # do 100 steps
+    while test.iter < test.maxit:
+        test.step()    
+    f2_avr += test.fhist
+    print('trial',trial,' minval',test.fhist[-1])
+
+f_avr /= ntrials
+f2_avr /= ntrials
+ 
+plt.semilogy(f_avr,label='Stars')
+plt.semilogy(f2_avr, label='Astars')
+plt.legend()
+plt.show()
